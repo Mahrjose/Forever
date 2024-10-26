863aef9ae74b0bd9c93987a2685a85ca1c1a8914
Mirza Mahrab Hossain <mahrjose.dev@gmail.com>
2024-10-26 22:21:47 +0600

    create react-app with vite

---
+++ frontend/src/App.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Collection from "./pages/Collection";
+import Product from "./pages/Product";
+import Cart from "./pages/Cart";
+import Orders from "./pages/Orders";
+import PlaceOrder from "./pages/PlaceOrder";
+
+const App = () => {
+  return (
+    <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/Contact" element={<Contact />} />
+        <Route path="/collection" element={<Collection />} />
+        <Route path="/Product/:productId" element={<Product />} />
+        <Route path="/Cart" element={<Cart />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/place-order" element={<PlaceOrder />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
